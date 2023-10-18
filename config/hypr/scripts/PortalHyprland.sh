801e3f82f4d9ad89f1b046ca01554fd055b1c049
JaKooLit <jimmielovejay@gmail.com>
2023-10-19 01:49:48 +0900

    updated portal hyprland script

--- config/hypr/scripts/PortalHyprland.sh
+++ config/hypr/scripts/PortalHyprland.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 sleep 1
 killall xdg-desktop-portal-hyprland
+killall xdg-desktop-portal-gnome
 killall xdg-desktop-portal-wlr
 killall xdg-desktop-portal
-/usr/lib/xdg-desktop-portal-hyprland &
+/usr/libexec/xdg-desktop-portal-hyprland &
 sleep 2
-/usr/lib/xdg-desktop-portal &
+/usr/libexec/xdg-desktop-portal &
