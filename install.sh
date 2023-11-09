cf2c29de42358e2a94fd901f09c01ed4df3a15de
JaKooLit <jimmielovejay@gmail.com>
2023-11-09 10:53:38 +0900

    just a small change

--- install.sh
+++ install.sh
@@ -118,7 +118,7 @@ ask_yes_no "-Installing on Asus ROG Laptops?" rog
 printf "\n"
 ask_yes_no "-Install and configure SDDM log-in Manager?" sddm
 printf "\n"
-ask_yes_no "-Install XDG-DESKTOP-PORTAL-HYPRLAND? (recommended for proper Screen Share ie OBS)" xdph
+ask_yes_no "Install XDG-DESKTOP-PORTAL-HYPRLAND? (for proper Screen Share ie OBS)" xdph
 printf "\n"
 ask_yes_no "-Do you want to install zsh and oh-my-zsh?" zsh
 printf "\n"
