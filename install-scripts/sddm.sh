837702431535d23223be403483ea8fc50151d40d
Ja.KooLit <ejhay.games@gmail.com>
2023-12-16 18:26:09 +0900

    Change SDDM install script re Theme

--- install-scripts/sddm.sh
+++ install-scripts/sddm.sh
@@ -84,13 +84,47 @@ sudo cp assets/hyprland.desktop "$wayland_sessions_dir/" 2>&1 | tee -a "$LOG"
 
 clear
     
-  # SDDM-themes
+# SDDM-themes
+valid_input=false
+while [ "$valid_input" != true ]; do
   read -n 1 -r -p "${CAT} OPTIONAL - Would you like to install SDDM themes? (y/n)" install_sddm_theme
   if [[ $install_sddm_theme =~ ^[Yy]$ ]]; then
-    printf "\n%s - Installing Tokyo SDDM Theme\n" "${NOTE}"
-    git clone https://github.com/rototrash/tokyo-night-sddm.git 2>&1 | tee -a "$LOG"
-    sudo mv tokyo-night-sddm /usr/share/sddm/themes/
-    echo -e "[Theme]\nCurrent=tokyo-night-sddm" | sudo tee -a "$sddm_conf_dir/10-theme.conf" 2>&1 | tee -a "$LOG"              		
+    printf "\n%s - Installing Simple SDDM Theme\n" "${NOTE}"
+
+    # Check if /usr/share/sddm/themes/simple-sddm exists and remove if it does
+    if [ -d "/usr/share/sddm/themes/simple-sddm" ]; then
+      sudo rm -rf "/usr/share/sddm/themes/simple-sddm"
+      echo -e "\e[1A\e[K${OK} - Removed existing 'simple-sddm' directory." 2>&1 | tee -a "$LOG"
+    fi
+
+    # Check if simple-sddm directory exists in the current directory and remove if it does
+    if [ -d "simple-sddm" ]; then
+      rm -rf "simple-sddm"
+      echo -e "\e[1A\e[K${OK} - Removed existing 'simple-sddm' directory from the current location." 2>&1 | tee -a "$LOG"
+    fi
+
+    if git clone https://github.com/JaKooLit/simple-sddm.git 2>&1 | tee -a "$LOG"; then
+      while [ ! -d "simple-sddm" ]; do
+        sleep 1
+      done
+
+      if [ ! -d "/usr/share/sddm/themes" ]; then
+        sudo mkdir -p /usr/share/sddm/themes
+        echo -e "\e[1A\e[K${OK} - Directory '/usr/share/sddm/themes' created." 2>&1 | tee -a "$LOG"
+      fi
+
+      sudo mv simple-sddm /usr/share/sddm/themes/
+      echo -e "[Theme]\nCurrent=simple-sddm" | sudo tee -a "$sddm_conf_dir/10-theme.conf" &>> "$LOG"
+    else
+      echo -e "\e[1A\e[K${ERROR} - Failed to clone the theme repository. Please check your internet connection or repository availability." | tee -a "$LOG" >&2
+    fi
+    valid_input=true
+  elif [[ $install_sddm_theme =~ ^[Nn]$ ]]; then
+    printf "\n%s - No SDDM themes will be installed.\n" "${NOTE}" 2>&1 | tee -a "$LOG"
+    valid_input=true
+  else
+    printf "\n%s - Invalid input. Please enter 'y' for Yes or 'n' for No.\n" "${ERROR}" 2>&1 | tee -a "$LOG"
   fi
+done
 
 clear
