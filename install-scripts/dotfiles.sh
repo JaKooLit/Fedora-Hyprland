4d2c8fe4b3c0cdd071533c35c813f1d4d9fe7c9d
Ja.KooLit <85185940+JaKooLit@users.noreply.github.com>
2024-08-10 02:45:24 +0000

    Update dotfiles.sh

--- install-scripts/dotfiles.sh
+++ install-scripts/dotfiles.sh
@@ -7,7 +7,7 @@
 source "$(dirname "$(readlink -f "$0")")/Global_functions.sh"
 
 # Check if Hyprland-Dots exists
-printf "${NOTE} Downloading Hyprland dots from main for the Hyprland v0.42.0 update...\n"
+printf "Downloading Hyprland dots from main for the Hyprland v0.42.0 update...\n"
 
 if [ -d Hyprland-Dots ]; then
   cd Hyprland-Dots
@@ -15,14 +15,14 @@ if [ -d Hyprland-Dots ]; then
   git pull
   git stash apply
   chmod +x copy.sh
-  ./copy.sh 2>&1 | tee -a "$LOG"
+  ./copy.sh 
 else
   if git clone --depth 1 https://github.com/JaKooLit/Hyprland-Dots; then
     cd Hyprland-Dots || exit 1
     chmod +x copy.sh
-    ./copy.sh 2>&1 | tee -a "$LOG"
+    ./copy.sh 
   else
-    echo -e "${ERROR} Can't download Hyprland-Dots" 2>&1 | tee -a "$LOG"
+    echo -e "Can't download Hyprland-Dots"
   fi
 fi
 
