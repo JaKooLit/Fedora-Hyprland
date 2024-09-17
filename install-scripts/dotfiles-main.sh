0488eb83d4d7dd943c963a9e4d78192b231c6363
JaKooLit <ejhay.games@gmail.com>
2024-09-18 01:20:43 +0900

    added dotfiles-main.sh to download from main instead of from releases

--- install-scripts/dotfiles-main.sh
+++ install-scripts/dotfiles-main.sh
@@ -7,7 +7,7 @@
 source "$(dirname "$(readlink -f "$0")")/Global_functions.sh"
 
 # Check if Hyprland-Dots exists
-printf "Downloading Hyprland dots from main for the Hyprland v0.42.0 update...\n"
+printf "${NOTE} Downloading KooL's Hyprland Dots....\n"
 
 if [ -d Hyprland-Dots ]; then
   cd Hyprland-Dots
@@ -22,7 +22,7 @@ else
     chmod +x copy.sh
     ./copy.sh 
   else
-    echo -e "Can't download Hyprland-Dots"
+    echo -e "$ERROR Can't download Hyprland-Dots"
   fi
 fi
 
