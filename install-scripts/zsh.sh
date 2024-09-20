99342d42878ec1bc8c0d9821d5bd8a89f518d600
JaKooLit <ejhay.games@gmail.com>
2024-09-20 17:43:09 +0900

    updated zsh.sh

--- install-scripts/zsh.sh
+++ install-scripts/zsh.sh
@@ -54,8 +54,8 @@ while true; do
             fi
             sed -i '/#pokemon-colorscripts --no-title -s -r/s/^#//' assets/.zshrc >> "$LOG" 2>&1
 
-            # fastfetch. Will be disabled if above colorscript was chosen to install
-            fastfetch -c $HOME/.config/fastfetch/config-compact.jsonc
+			# commenting out fastfetch since pokemon was chosen to install
+            sed -i '/^fastfetch -c $HOME\/.config\/fastfetch\/config-compact.jsonc/s/^/#/' assets/.zshrc >> "$LOG" 2>&1
             
 			echo "${NOTE} Pokemon Installation process completed" 2>&1 | tee -a "$LOG"
             break
