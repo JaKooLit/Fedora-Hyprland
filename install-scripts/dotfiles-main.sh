203deb85c43b04b9eed1eadf2e6f3ba162f21c1f
JaKooLit <ejhay.games@gmail.com>
2025-02-04 22:24:10 +0900

    updated showcase readme

--- install-scripts/dotfiles-main.sh
+++ install-scripts/dotfiles-main.sh
@@ -6,9 +6,9 @@
 
 source "$(dirname "$(readlink -f "$0")")/Global_functions.sh"
 
-# Check if Hyprland-Dots exists
-printf "${NOTE} Cloning and Installing ${SKY_BLUE}KooL's Hyprland Dots${RESET}....\n"
 
+printf "${NOTE} Cloning and Installing ${SKY_BLUE}KooL's Hyprland Dots${RESET}....\n"
+# Check if Hyprland-Dots exists
 if [ -d Hyprland-Dots ]; then
   cd Hyprland-Dots
   git stash
