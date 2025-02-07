4978405a509c411d3cd6b75d8ba9694134484745
JaKooLit <ejhay.games@gmail.com>
2025-02-07 21:27:51 +0900

    updated sddm and sddm theme

--- install-scripts/sddm.sh
+++ install-scripts/sddm.sh
@@ -43,11 +43,6 @@ printf " Activating sddm service........\n"
 sudo systemctl set-default graphical.target 2>&1 | tee -a "$LOG"
 sudo systemctl enable sddm.service 2>&1 | tee -a "$LOG"
 
-# Set up SDDM
-echo -e "${NOTE} Setting up the login screen."
-sddm_conf_dir=/etc/sddm.conf.d
-[ ! -d "$sddm_conf_dir" ] && { printf "$CAT - $sddm_conf_dir not found, creating...\n"; sudo mkdir -p "$sddm_conf_dir" 2>&1 | tee -a "$LOG"; }
-
 wayland_sessions_dir=/usr/share/wayland-sessions
 [ ! -d "$wayland_sessions_dir" ] && { printf "$CAT - $wayland_sessions_dir not found, creating...\n"; sudo mkdir -p "$wayland_sessions_dir" 2>&1 | tee -a "$LOG"; }
 sudo cp assets/hyprland.desktop "$wayland_sessions_dir/" 2>&1 | tee -a "$LOG"
