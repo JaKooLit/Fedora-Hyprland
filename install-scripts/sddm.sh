52e452508135af743d852b8e0934237670d17c2c
JaKooLit <ejhay.games@gmail.com>
2025-02-09 08:42:54 +0900

    updated sddm.sh

--- install-scripts/sddm.sh
+++ install-scripts/sddm.sh
@@ -31,7 +31,7 @@ for PKG2 in "${sddm[@]}"; do
 done
 
 # Check if other login managers are installed and disabling their service before enabling sddm
-for login_manager in lightdm gdm3 gdm lxdm xdm lxdm-gtk3; do
+for login_manager in lightdm gdm3 gdm lxdm lxdm-gtk3; do
   if sudo dnf list installed "$login_manager" > /dev/null; then
     echo "disabling $login_manager..."
     sudo systemctl disable "$login_manager.service" >> "$LOG" 2>&1
