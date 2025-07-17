0d411f9f0920abc007b12948744054d90daec6d4
JaKooLit <ejhay.games@gmail.com>
2025-07-17 20:45:43 +0900

    added to ran quickshell on qs script

--- install-scripts/quickshell.sh
+++ install-scripts/quickshell.sh
@@ -41,4 +41,7 @@ for pkg in "${quick[@]}"; do
     install_package "$pkg" "$LOG"
 done
 
+# starting quickshell so KooL Hyprland Dots Quickshell config can be copied
+qs || true
+
 printf "\n%.0s" {1..1}
